5b48dcbcc408a589d17d8cb88c29d6d79e40ce12
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-15 18:25:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,11 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday_name_hashes|
   puts "#{season.to_s.capitalize!}:"
   holiday_name_hashes.each do |holiday_name, supply_array|
-  puts "  #{holiday_name.to_s.split(' ' && '_').map(&:capitalize).join(' ')}: #{supply_array.to_s.gsub!(/[^A-Za-z, ]/, '')}"
+  puts "  #{holiday_name.to_s.split('_').map(&:capitalize).join(' ')}: #{supply_array.to_s.gsub!(/[^A-Za-z, ]/, '')}"
+  
+       end
+    end
+end
 end
 end
 end
